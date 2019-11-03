9db7773981b4932ed21c6ff485b7fd83ceef2094
Colin Richardson <colinr@mbmsoftwareltd.co.uk>
2019-11-03 09:10:15 +0000

    Added the Navbar and React Router

---
+++ src/containers/Home.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Home.css";
+
+export default function Home() {
+    return (
+        <div className="Home">
+            <div className="lander">
+                <h1>Traveller Patrons</h1>
+                <p>Find Patrons for any encounter in the Universe!</p>
+            </div>
+        </div>
+    );
+}
