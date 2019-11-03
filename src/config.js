c3b6cbb3b9cd0db2be8c050a7871ccee7e09c739
Colin Richardson <colinr@mbmsoftwareltd.co.uk>
2019-11-03 09:50:09 +0000

    Added all the settings

---
+++ src/config.js
@@ -0,0 +1,15 @@
+export default {
+  s3: {
+    REGION: "eu-west-1",BUCKET: "travellerapp-uploads"
+  },
+  apiGateway: {
+    REGION: "eu-west-1",
+    URL: "https://ersdhfmit7.execute-api.eu-west-1.amazonaws.com/prod"
+  },
+  cognito: {
+    REGION: "eu-west-1",
+    USER_POOL_ID: "eu-west-1_ygnRqwWx8",
+    APP_CLIENT_ID: "5487cn55sg6jotlj3cf0g2gln2",
+    IDENTITY_POOL_ID: "eu-west-1:735a5f29-bd42-4fcd-9f0e-dfb43e18f959"
+  }
+};
